f492d766f5f7a4dc4e2d69e158309990265a5391
Krone <X10090916@gmail.com>
2022-03-18 09:57:53 +0800

    fix(lcof58.t2): my mistake write lcof58.t2 as t1

---
+++ swordpointing2offer/src/lcof58/t2/Solution.java
@@ -0,0 +1,13 @@
+package lcof58.t2;
+
+/**
+ * 剑指 Offer 58 - II. 左旋转字符串
+ * https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof/
+ * @author xzq
+ * @create 2022-03-08-8:55
+ */
+class Solution {
+    public String reverseLeftWords(String s, int n) {
+        return s.substring(n, s.length()) + s.substring(0, n);
+    }
+}
