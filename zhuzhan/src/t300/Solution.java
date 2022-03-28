f9b0311749e530bbd0b6e1a6bf5a23dd65372622
Krone <X10090916@gmail.com>
2022-03-28 21:35:58 +0800

    feat(t300): Longest Increasing Subsequence, LIS dp O(nlogn)

--- zhuzhan/src/t300/Solution.java
+++ zhuzhan/src/t300/Solution.java
@@ -8,21 +8,51 @@ import java.util.Arrays;
  * @author xzq
  * @create 2022-03-28-21:01
  */
+// O(n^2)
+//class Solution {
+//    public int lengthOfLIS(int[] nums) {
+//        int n = nums.length;
+//        int ans = 0;
+//        int[] dp = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            dp[i] = 1;
+//            for (int j = i-1; j >= 0; j--) {
+//                if (nums[i] > nums[j]) {
+//                    dp[i] = Math.max(dp[i], dp[j]+1);
+//                }
+//            }
+//            ans = Math.max(ans, dp[i]);
+//        }
+//        System.out.println(Arrays.toString(dp));
+//        return ans;
+//    }
+//}
+// O(nlogn) 贪心+二分
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int ans = 0;
-        int[] dp = new int[n];
-        for (int i = 0; i < n; i++) {
-            dp[i] = 1;
-            for (int j = i-1; j >= 0; j--) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j]+1);
-                }
-            }
-            ans = Math.max(ans, dp[i]);
+        if (n == 0) {
+            return 0;
         }
-        System.out.println(Arrays.toString(dp));
-        return ans;
+        int len = 1;
+        int[] dp = new int[n+1];
+        dp[len] = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > dp[len]) {
+                dp[++len] = nums[i];
+            } else {
+                int s = 1, t = len+1;
+                while (s < t) {
+                    int m = (s+t) >> 1;
+                    if (dp[m] >= nums[i]) {
+                        t = m;
+                    } else {
+                        s = m+1;
+                    }
+                }
+                dp[s] = nums[i];
+            }
+        }
+        return len;
     }
 }
