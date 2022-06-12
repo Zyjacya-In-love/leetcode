03a49da5145dd78cd336fda88c978e867fe212ca
Krone <X10090916@gmail.com>
2022-06-12 23:55:17 +0800

    feat(t518,377): complete bag from programmercarl.com chapter 10.dp 2022.6.12

--- zhuzhan/src/t518/Solution.java
+++ zhuzhan/src/t518/Solution.java
@@ -30,21 +30,38 @@ import java.util.Arrays;
 // 通过   5 ms	48 MB
 
 // 滚动数组优化空间
+//class Solution {
+//    public int change(int amount, int[] coins) {
+//        int n = coins.length;
+//        int[] dp = new int[amount+1];
+//        dp[0] = 1;
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= amount; j++) {
+//                if (j >= coins[i-1]) {
+//                    dp[j] = dp[j] + dp[j-coins[i-1]];
+//                } else {
+//                    dp[j] = dp[j];
+//                }
+//            }
+//        }
+//        return dp[amount];
+//    }
+//}
+// 通过   5 ms	38.9 MB
+
+
+/*
+ * @create 2022-06-12-10:52
+ */
 class Solution {
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        int[] dp = new int[amount+1];
+        int[] dp = new int[amount + 1];
         dp[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= amount; j++) {
-                if (j >= coins[i-1]) {
-                    dp[j] = dp[j] + dp[j-coins[i-1]];
-                } else {
-                    dp[j] = dp[j];
-                }
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = coins[i]; j <= amount; j++) {
+                dp[j] += dp[j - coins[i]];
             }
         }
         return dp[amount];
     }
 }
-// 通过   5 ms	38.9 MB
