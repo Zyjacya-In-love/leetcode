27f0c757a1d9472f6cc681f3eced2b32119f0db7
Krone <X10090916@gmail.com>
2022-06-26 23:54:15 +0800

    feat(t322): complete bag scroll array from programmercarl.com chapter 10.dp 2022.6.26

--- zhuzhan/src/t322/Solution.java
+++ zhuzhan/src/t322/Solution.java
@@ -8,19 +8,40 @@ import java.util.Arrays;
  * @author xzq
  * @create 2022-06-13-23:52
  */
+//class Solution {
+//    public int coinChange(int[] coins, int amount) {
+//        int[] dp = new int[amount + 1];
+//        Arrays.fill(dp, Integer.MAX_VALUE);
+//        dp[0] = 0;
+//        for (int i = 0; i < coins.length; i++) {
+//            for (int j = coins[i]; j <= amount; j++) {
+//                if (dp[j - coins[i]] != Integer.MAX_VALUE) {
+//                    dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+//                }
+//            }
+//        }
+//        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+//
+//    }
+//}
+
+
+/*
+ * @create 2022-06-26-23:45
+ */
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
+        int[] dp = new int[amount+1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
         for (int i = 0; i < coins.length; i++) {
             for (int j = coins[i]; j <= amount; j++) {
-                if (dp[j - coins[i]] != Integer.MAX_VALUE) {
-                    dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+                if (dp[j-coins[i]] != Integer.MAX_VALUE) {
+                    dp[j] = Math.min(dp[j], dp[j-coins[i]]+1);
                 }
             }
         }
         return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
-
     }
 }
+// 通过   11 ms	41.3 MB
