c8a85fc7e009d2dc1ec41ebdf1d4e29af1340dc6
Krone <X10090916@gmail.com>
2022-10-04 15:16:56 +0800

    fix(t921): leetcode everyday 2022.10.4

--- zhuzhan/src/t921/Solution.java
+++ zhuzhan/src/t921/Solution.java
@@ -6,22 +6,48 @@ package t921;
  * @author xzq
  * @create 2022-05-13-15:27
  */
+//class Solution {
+//    public int minAddToMakeValid(String s) {
+//        int leftNeed = 0;
+//        int rightNeed = 0;
+//        for (int i = 0; i < s.length(); i++) {
+//            if (s.charAt(i) == '(') {
+//                rightNeed++;
+//            } else {
+//                rightNeed--;
+//                if (rightNeed == -1) {
+//                    leftNeed++;
+//                    rightNeed = 0;
+//                }
+//            }
+//        }
+//        return leftNeed+rightNeed;
+//    }
+//}
+// 通过   0 ms	39.5 MB
+
+
+/*
+ * @create 2022-10-04-15:05
+ */
+// 遍历字符串的同时计数 左括号 left 的数量，如果当前是右括号，则抵消一个左括号，如果之前没有左括号，则将左括号需求值+1，
+// 遍历结束后当前左括号的数量即是所需右括号的数量，最后返回 左右括号的需求值的和
 class Solution {
     public int minAddToMakeValid(String s) {
+        int left = 0;
         int leftNeed = 0;
-        int rightNeed = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                rightNeed++;
+            char ch = s.charAt(i);
+            if (ch == '(') {
+                left++;
             } else {
-                rightNeed--;
-                if (rightNeed == -1) {
+                if (left == 0) {
                     leftNeed++;
-                    rightNeed = 0;
+                } else {
+                    left--;
                 }
             }
         }
-        return leftNeed+rightNeed;
+        return left+leftNeed;
     }
 }
-// 通过   0 ms	39.5 MB
